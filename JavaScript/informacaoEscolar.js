a4e0f1fdbf7d5cd7376144857b23f3e3916569d2
root <afl5@discente.ifpe.edu.br>
2024-07-06 03:08:17 -0300

    Envia arquivos do projeto integrador

---
+++ JavaScript/informacaoEscolar.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+const image = document.getElementById('iconeSair');
+
+image.addEventListener('click', () => {
+    window.location.href = '/home/usuario/Projeto_APAE_PALMARES/Html/Login.html'; 
+});
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+const image = document.getElementById('iconeVoltar');
+
+image.addEventListener('click', () => {
+    window.location.href = '/home/usuario/Projeto_APAE_PALMARES/Html/InfoSocioeconomicas.html';
+ 
+});
+});
